c6ed2f3ace96bcd7189318d3b83b2b2390b3b56f
ethandang1 <58610660+ethandang1@users.noreply.github.com>
2020-02-25 13:37:19 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,3 @@
+game.splash("TRUE: IT'S WEDNESDAY" + "TRUE: THERE IS NO HOLIDAY" + "YOU GO TO SCHOOL")
+game.splash("One thing is true for something to happen:" + "True: I eat" + "False: I eat a lot" + "Result: I'm full")
+game.splash("One thing is true for something to not happen:" + "True: It's summer" + "Result: There is no school")
